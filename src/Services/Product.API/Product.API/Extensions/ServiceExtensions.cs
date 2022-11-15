906dc25a95c0a5c75c91ca9a3264410e4dda4d17
Nam Dinh <namqd98@gmail.com>
2022-11-15 20:40:59 +0700

    add config product api

--- src/Services/Product.API/Product.API/Extensions/ServiceExtensions.cs
+++ src/Services/Product.API/Product.API/Extensions/ServiceExtensions.cs
@@ -2,13 +2,13 @@
 {
     public static class ServiceExtensions
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services) 
         {
             services.AddControllers();
             services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
-
             return services;
         }
     }
--- src/Services/Product.API/Product.API/Program.cs
+++ src/Services/Product.API/Product.API/Program.cs
@@ -4,25 +4,30 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Log.Information("Starting product API up");
+Log.Information("Start Product API up");
 
 try
 {
     builder.Host.UseSerilog(Serilogger.Configure);
     builder.Host.AddAppConfigurations();
-
+    // Add services to the container.
     builder.Services.AddInfrastructure();
 
     var app = builder.Build();
-
+    app.UseInfrastructure();
     app.Run();
-
 }
+
 catch (Exception ex)
 {
-    Log.Fatal(ex, "Unhandle exception");
-}
+    string type = ex.GetType().Name;
+    if (type.Equals("StopTheHostException", StringComparison.Ordinal))
+    {
+        throw;
+    }
 
+    Log.Fatal(ex, $"Unhandled exception: {ex.Message}");
+}
 finally
 {
     Log.Information("Shut down Product API complete");
